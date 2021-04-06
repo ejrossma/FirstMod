e5eec76119b369d982f5d553fd5b6ea4c97734dc
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-06 13:49:28 -0700

    Added obsidian bat

--- src/main/java/vulgit/first/mod/registry/ModItems.java
+++ src/main/java/vulgit/first/mod/registry/ModItems.java
@@ -9,10 +9,11 @@ public class ModItems {
 
     //Items
     public static final Item BAT = new Item(new Item.Settings().group(FirstMod.ITEM_GROUP));
-
+    public static final Item OBSIDIANBAT = new Item(new Item.Settings().group(FirstMod.ITEM_GROUP));
     //BlockItems
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("firstmod", "bat"), BAT);
+        Registry.register(Registry.ITEM, new Identifier("firstmod", "obsidian_bat"), OBSIDIANBAT);
     }
 }
