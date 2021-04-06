5dc24034a16112c589e583ba6f62a69b52b39c10
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-06 14:54:38 -0700

    Added effect to obsidian bat

---
+++ src/main/java/vulgit/first/mod/BatItem.java
@@ -0,0 +1,22 @@
+package vulgit.first.mod;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class BatItem extends Item {
+    public BatItem(Settings item$Settings_1) {
+        super(item$Settings_1);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity PlayerEntity, Hand hand){
+        PlayerEntity.playSound(SoundEvents.ENTITY_DOLPHIN_DEATH, 1.5F, 0.5F);
+        return new TypedActionResult<ItemStack>(ActionResult.SUCCESS, PlayerEntity.getStackInHand(hand));
+    }
+}
--- src/main/java/vulgit/first/mod/registry/ModItems.java
+++ src/main/java/vulgit/first/mod/registry/ModItems.java
@@ -1,15 +1,18 @@
 package vulgit.first.mod.registry;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import vulgit.first.mod.BatItem;
 import vulgit.first.mod.FirstMod;
 
 public class ModItems {
 
     //Items
     public static final Item BAT = new Item(new Item.Settings().group(FirstMod.ITEM_GROUP));
-    public static final Item OBSIDIANBAT = new Item(new Item.Settings().group(FirstMod.ITEM_GROUP));
+    public static final Item OBSIDIANBAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
+    //("obsidian_bat", 0, 800, 6.0F, 6.5F, 15);
     //BlockItems
 
     public static void registerItems() {
