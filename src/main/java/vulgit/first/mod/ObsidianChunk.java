6665fca345fbfebdfff6a081a7a4c050e318309e
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-06 23:47:13 -0700

    Changed obsidian bat to be sword type

---
+++ src/main/java/vulgit/first/mod/ObsidianChunk.java
@@ -0,0 +1,34 @@
+package vulgit.first.mod;
+
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class ObsidianChunk implements ToolMaterial {
+    public static final ObsidianChunk INSTANCE = new ObsidianChunk();
+    @Override
+    public int getDurability() {
+        return 800;
+    }
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 1.0F;
+    }
+    @Override
+    public float getAttackDamage() {
+        return 3.0F;
+    }
+    @Override
+    public int getMiningLevel() {
+        return 1;
+    }
+    @Override
+    public int getEnchantability() {
+       return 10;
+    }
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.POTATO);
+    }
+}
--- src/main/java/vulgit/first/mod/registry/ModItems.java
+++ src/main/java/vulgit/first/mod/registry/ModItems.java
@@ -1,18 +1,19 @@
 package vulgit.first.mod.registry;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import vulgit.first.mod.BatItem;
 import vulgit.first.mod.FirstMod;
+import vulgit.first.mod.ObsidianChunk;
 
 public class ModItems {
 
     //Items
     public static final Item BAT = new Item(new Item.Settings().group(FirstMod.ITEM_GROUP));
-    public static final Item OBSIDIANBAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
-    //("obsidian_bat", 0, 800, 6.0F, 6.5F, 15);
+    public static ToolItem OBSIDIANBAT = new SwordItem(ObsidianChunk.INSTANCE, 3, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
     //BlockItems
 
     public static void registerItems() {
