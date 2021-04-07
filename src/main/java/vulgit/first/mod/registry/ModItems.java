e0e9402a296eb5aa609b2c9675d07fc0085b7639
ejrossma <ejrossma@ucsc.edu>
2021-04-06 23:59:20 -0700

    Added crafting files but haven't given recipes yet

--- src/main/java/vulgit/first/mod/registry/ModItems.java
+++ src/main/java/vulgit/first/mod/registry/ModItems.java
@@ -10,13 +10,13 @@ import vulgit.first.mod.FirstMod;
 public class ModItems {
 
     //Items
-    public static final Item BAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
-    public static final Item OBSIDIANBAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
+    public static final Item WOODEN_BAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
+    public static final Item OBSIDIAN_BAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
     //("obsidian_bat", 0, 800, 6.0F, 6.5F, 15);
     //BlockItems
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier("firstmod", "bat"), BAT);
-        Registry.register(Registry.ITEM, new Identifier("firstmod", "obsidian_bat"), OBSIDIANBAT);
+        Registry.register(Registry.ITEM, new Identifier("firstmod", "bat"), WOODEN_BAT);
+        Registry.register(Registry.ITEM, new Identifier("firstmod", "obsidian_bat"), OBSIDIAN_BAT);
     }
 }
