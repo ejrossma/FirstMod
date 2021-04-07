c04d9d3cb15c9f0596d3ea2b06aac0215b9240d9
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-07 00:27:17 -0700

    Changed wooden bat to sworditem

--- src/main/java/vulgit/first/mod/registry/ModItems.java
+++ src/main/java/vulgit/first/mod/registry/ModItems.java
@@ -6,19 +6,17 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import vulgit.first.mod.tools.BatItem;
-import vulgit.first.mod.FirstMod;
 import vulgit.first.mod.tools.materials.ObsidianChunk;
 
 public class ModItems {
 
     //Items
-    public static final Item WOODEN_BAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
-    public static ToolItem OBSIDIAN_BAT = new SwordItem(ObsidianChunk.INSTANCE, 1, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final ToolItem WOODEN_BAT = new SwordItem(ObsidianChunk.INSTANCE, 1, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final ToolItem OBSIDIAN_BAT = new SwordItem(ObsidianChunk.INSTANCE, 1, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
     //BlockItems
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier("firstmod", "bat"), WOODEN_BAT);
+        Registry.register(Registry.ITEM, new Identifier("firstmod", "wooden_bat"), WOODEN_BAT);
         Registry.register(Registry.ITEM, new Identifier("firstmod", "obsidian_bat"), OBSIDIAN_BAT);
     }
 }
