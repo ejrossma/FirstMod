0489231db10ea8ac78d9ed901258e44aa2c6acf5
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-07 00:16:45 -0700

    Made obsidian weapon and extra function

--- src/main/java/vulgit/first/mod/registry/ModItems.java
+++ src/main/java/vulgit/first/mod/registry/ModItems.java
@@ -13,12 +13,12 @@ import vulgit.first.mod.ObsidianChunk;
 public class ModItems {
 
     //Items
-    public static final Item BAT = new BatItem(new Item.Settings().group(FirstMod.ITEM_GROUP));
-    public static ToolItem OBSIDIANBAT = new SwordItem(ObsidianChunk.INSTANCE, 1, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item BAT = new Item(new Item.Settings().group(FirstMod.ITEM_GROUP));
+    public static ToolItem OBSIDIANBAT = new SwordItem(ObsidianChunk.INSTANCE, 2, -2.4F, new Item.Settings().group(ItemGroup.COMBAT));
     //BlockItems
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("firstmod", "bat"), BAT);
-        Registry.register(Registry.ITEM, new Identifier("firstmod", "obsidian_bat"), OBSIDIANBAT);
+        Registry.register(Registry.ITEM, new Identifier("firstmod", "obsidian_bat"), new BatItem(new ObsidianChunk()));
     }
 }
